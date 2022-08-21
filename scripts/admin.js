fd3c82dd7b993cdf4de4235ca6fdb385f766e940
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-08-22 00:41:19 +0530

    idk

---
+++ scripts/admin.js
@@ -0,0 +1,49 @@
+let admin ={
+ email:"nitinsharma754010@gmail.com",
+ name: 'nitin'
+
+}
+
+
+let submit = async () => {
+  let user_details = JSON.parse(localStorage.getItem("user_details"));
+  let user = await getprofile(user_details.username, user_details.token);
+  if (user.email !== admin.email) {
+    alert("not valid");
+    return;
+  }
+
+  let iteam = {
+    title: document.getElementById("title").value,
+    image: document.getElementById("image").value,
+    price: document.getElementById("price").value,
+  };
+
+  let res = await fetch(`http://localhost:3000/api/menu`, {
+    method: "POST",
+
+    body: JSON.stringify(iteam),
+
+    //--> we are telling what type of data we are sending into local server
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  let data = await res.json();
+  console.log(data);
+};
+
+let getprofile = async (username, token) => {
+  let res = await fetch(
+    `https://masai-api-mocker.herokuapp.com/user/${username}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  let data = await res.json();
+  return data;
+};
--- scripts/signup.js
+++ scripts/signup.js
@@ -36,6 +36,9 @@ async function login() {
     password: document.getElementById("lpassword").value,
   };
 
+  //--> we taking direct else it gives error
+  let user=login_data.username;
+
   let res = await fetch(`https://masai-api-mocker.herokuapp.com/auth/login`, {
     method: "POST",
 
@@ -48,21 +51,39 @@ async function login() {
   });
 
   //--> passing this in get profile data
-  getprofile(login_data.username, login_data.password)
+  
+
+  savetoken(user,data.token,10000)
+
+
   let data = await res.json();
   console.log(data);
 }
 
 
-//--> for varify token
-let getprofile = async (username, token) => {
-  let res = await fetch(`https://masai-api-mocker.herokuapp.com/user/${username}`, {
-   
-  headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+// //--> for varify token
 
-  let data = await res.json();
-  console.log(data);
-};
+//-->making seassion storage means after a time letter seassion got expires
+let savetoken = (username,token,time)=>{
+
+  let user={
+      username,
+      token
+  }
+
+  localStorage.setItem('user_details', JSON.stringify(token));
+  setTimeout(()=>{
+   
+      localStorage.setItem('user_details', JSON.stringify(null));
+ 
+
+  },time)
+}
+
+
+
+// ! flow of the code
+//* singup
+//* login
+//* using token get profile details
+//! based on user details we can check user is admin or not
